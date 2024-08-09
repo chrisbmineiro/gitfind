2e0a6f3a74fe629bbd1471b8a5362f6235190b1b
Christopher <chrisbmineiro@gmail.com>
2024-08-08 22:54:42 -0300

    deixando o layout dinamico de acordo com a busca atraves de uma API do GitHub

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import { React, useState } from 'react'
 import { Header } from '../../components/Header'
 import background from '../../assets/background.png'
 import './styles.css'
-import Card from '../../components/Card';
 import ItemList from '../../components/ItemList';
 
 
 function App() {
+  const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState([]);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    if (newUser.name) {
+      const { avatar_url, name, login, bio } = newUser;
+      setCurrentUser({ avatar_url, name, bio, login });
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+      const newRepos = await reposData.json();
+
+      if (newRepos.length > 0) {
+        setRepos(newRepos);
+      }
+    };
+  }
+
   return (
     <div className="App">
       <Header />
@@ -14,14 +34,32 @@ function App() {
         <img src={background} alt="background" className='background' />
         <div className='info'>
           <div>
-            <input name='user' placeholder='@usuario' />
-            <button>Buscar</button>'
+            <input
+              name='user' placeholder='@usuario'
+              value={user}
+              onChange={event => setUser(event.target.value)} />
+            <button onClick={handleGetData}>Buscar</button>'
           </div>
-          <Card user="Christopher Barros" username="@chrisbarros" description="Desenvolvedor Front-end" />
-          <h4>Repositórios</h4>
-          <ItemList title="fisiotheapp-challenge" description="Full Stack Developer web and mobile, passionate to javascript and all your ecosystem." />
-          <ItemList title="fisiotheapp-challenge" description="Full Stack Developer web and mobile, passionate to javascript and all your ecosystem." />
-          <ItemList title="fisiotheapp-challenge" description="Full Stack Developer web and mobile, passionate to javascript and all your ecosystem." />
+          {currentUser?.name ? (<>
+            <div className='perfil'>
+              <img src={currentUser.avatar_url} alt="foto de perfil" />
+              <div>
+                <h3>{currentUser.name}</h3>
+                <span>@{currentUser.login}</span>
+                <p>{currentUser.bio}</p>
+              </div>
+            </div>
+            <hr style={{ marginTop: '20px' }} />
+          </>
+          ) : null}
+          {repos?.length > 0 ? (
+            <div>
+              <h4>Repositórios</h4>
+              {repos.map(repo => (
+                <ItemList title={repo.name} description={repo.description} />
+              ))}
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
